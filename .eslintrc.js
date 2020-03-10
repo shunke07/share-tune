fc2367207387496e53467383c7f3341295534620
shunke07 <metagurosu0@gmail.com>
2020-03-11 00:17:52 +0900

    Update .eslintrc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,14 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'vue/singleline-html-element-content-newline': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: { void: 'always' }
+      }
+    ]
   }
 }
