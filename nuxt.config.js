1c580b419f741c37b051ec7d6d0cfed71d103da1
shunke07 <metagurosu0@gmail.com>
2020-03-11 02:08:22 +0900

    Update nuxt.config web app manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,6 +80,38 @@ export default {
    ** Nuxt.js modules
    */
   modules: ['@nuxtjs/pwa'],
+  //
+  // Web App manifest
+  //
+  manifest: {
+    name: SITE_NAME,
+    short_name: SITE_NAME,
+    lang: 'ja',
+    title: SITE_NAME,
+    'og:title': SITE_NAME,
+    description: SITE_DESC,
+    'og:description': SITE_DESC,
+    theme_color: '#007c91', // primary color
+    background_color: '#ffffff',
+    display: 'fullscreen',
+    icons: [
+      {
+        src: 'app/icons/icon@180px.png',
+        sizes: '180x180',
+        type: 'image/png'
+      },
+      {
+        src: 'app/icons/icon@192px.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: 'app/icons/icon@512px.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  },
   /*
    ** Build configuration
    */
@@ -89,7 +121,9 @@ export default {
      */
     extend(config, ctx) {}
   },
+  //
   // environment variables
+  //
   env: {
     FIREBASE_API_KEY: 'AIzaSyDYFQ0ofx0raM52hSNHVKTFDwG8DK5y2EY',
     FIREBASE_AUTH_DOMAIN: 'share-tune.firebaseapp.com',
