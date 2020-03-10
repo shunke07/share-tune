8530466dcb3a2173c0dc56f3e23654992b8da122
shunke07 <metagurosu0@gmail.com>
2020-03-11 00:17:01 +0900

    Add npm eslint-plugin-vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,11 @@ module.exports = {
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:vue/recommended'
   ],
   plugins: [
+    'vue',
     'prettier'
   ],
   // add your custom rules here
