835ca4242745035ef31f88818f575d046731ea66
shunke07 <metagurosu0@gmail.com>
2020-03-16 00:08:33 +0900

    Add components/BaseButton

---
+++ app/components/form/BaseButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="base-button"
+    :disabled="disabled"
+    @click.prevent="$emit('onClick')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-button {
+  @include strong;
+  height: 32px;
+  width: 100%;
+  max-width: $maxViewWidth;
+  padding: 6px 16px;
+  box-shadow: $shadowLow;
+  border-radius: 4px;
+  background: $primary;
+  color: $white;
+
+  &:disabled {
+    opacity: 0.5;
+  }
+}
+</style>
