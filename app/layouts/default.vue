e82707a17ce88b4dcc7c8e4ec53f848bdb243596
shunke07 <metagurosu0@gmail.com>
2020-03-15 21:37:52 +0900

    Update layout render loading components

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="only-sp-view">
     <nuxt />
+    <TheLoading v-show="isLoading" />
   </div>
 </template>
+
+<script>
+import TheLoading from '~/components/TheLoading'
+
+export default {
+  components: {
+    TheLoading
+  },
+
+  computed: {
+    isLoading() {
+      return this.$store.state.isLoading
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .only-sp-view {
