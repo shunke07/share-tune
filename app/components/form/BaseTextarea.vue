37724aafa2a03b96bdffe88567c8d17b8e1356da
shunke07 <metagurosu0@gmail.com>
2020-03-16 00:09:12 +0900

    Add components/BaseTextarea

---
+++ app/components/form/BaseTextarea.vue
@@ -0,0 +1,96 @@
+<template>
+  <label class="base-textarea">
+    <textarea
+      v-model="value"
+      class="textarea"
+      placeholder=" "
+      :maxlength="maxLength"
+    />
+    <span class="label">{{ label }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    currentValue: {
+      type: String,
+      default: ''
+    },
+    maxLength: {
+      type: Number,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      value: ''
+    }
+  },
+
+  watch: {
+    value(newVal) {
+      this.$emit('onChangeValue', newVal)
+    }
+  },
+
+  mounted() {
+    if (this.currentValue) this.value = this.currentValue
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-textarea {
+  position: relative;
+  width: 100%;
+  height: 120px;
+  max-width: $maxViewWidth;
+
+  > .textarea {
+    @include input;
+    resize: none;
+    width: 100%;
+    height: 100%;
+    caret-color: $primary;
+    border-bottom: 1px solid $lightGray;
+    margin-top: 16px;
+
+    &:hover {
+      border-bottom: 2px solid $black;
+    }
+
+    &:focus {
+      outline: none;
+      border-bottom: 2px solid $primary;
+
+      + .label {
+        color: $primary;
+        transform: translateY(-16px) scale(0.75);
+      }
+    }
+
+    &:not(:placeholder-shown) + .label {
+      transform: translateY(-16px) scale(0.75);
+    }
+  }
+
+  > .label {
+    @include input;
+    font-size: 14px;
+    top: 16px;
+    left: 0;
+    position: absolute;
+    color: $gray;
+    transform-origin: 0 0;
+    transform: translate3d(0, 0, 0);
+    transition: all 0.2s ease;
+    pointer-events: none;
+  }
+}
+</style>
