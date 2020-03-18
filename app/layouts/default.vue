b0d642aa707285ba6ae82f4a83a4121cae14f4c3
shunke07 <metagurosu0@gmail.com>
2020-03-18 22:49:07 +0900

    Update layouts import TheAppBar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,21 +1,48 @@
 <template>
   <div class="only-sp-view">
+    <header>
+      <TheAppBar v-show="isAppBarVisible" :title="pageTitle" />
+    </header>
     <nuxt v-show="!isLoading" />
     <TheLoading v-show="isLoading" />
   </div>
 </template>
 
 <script>
+import TheAppBar from '~/components/TheAppBar'
 import TheLoading from '~/components/TheLoading'
 
 export default {
   components: {
+    TheAppBar,
     TheLoading
   },
 
   computed: {
     isLoading() {
       return this.$store.state.isLoading
+    },
+
+    isAppBarVisible() {
+      const excludedPaths = ['/']
+      return !excludedPaths.includes(this.$route.path)
+    },
+
+    pageTitle() {
+      const { path } = this.$route
+
+      switch (path) {
+        case '/signup/':
+          return 'ユーザー登録'
+        case '/login/':
+          return 'ログイン'
+        case '/terms/':
+          return '利用規約'
+        case '/policy/':
+          return 'プライバシーポリシー'
+        default:
+          return ''
+      }
     }
   }
 }
