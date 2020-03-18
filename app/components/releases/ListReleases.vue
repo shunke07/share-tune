11afb1a1e2fcaad2629b58fe64ed06c14e8950be
shunke07 <metagurosu0@gmail.com>
2020-03-19 01:23:33 +0900

    Add components ListReleases

---
+++ app/components/releases/ListReleases.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class="list-releases">
+    <li
+      v-for="(release, index) in releases"
+      :key="`release-${index}`"
+      class="release-item"
+    >
+      <img
+        class="img"
+        :src="release.images[1].url"
+        :alt="`${release.name}の画像`"
+      />
+      <p class="title">
+        {{ release.name }}
+      </p>
+      <p class="artist">
+        {{ release.artists[0].name }}
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      releases: []
+    }
+  },
+
+  async mounted() {
+    this.$store.commit('setIsLoading', true)
+
+    const api = this.$functions.httpsCallable('spotifyGetNewReleases')
+    const result = await api().catch((error) => this.$nuxt.error(error))
+
+    this.$store.commit('setIsLoading', false)
+
+    if (!result) return
+    this.releases = result.data
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.list-releases {
+  display: flex;
+  flex-wrap: wrap;
+
+  > .release-item {
+    margin: 0 0 16px 16px;
+    width: calc(50% - 24px);
+
+    > .img {
+      width: 100%;
+      border: 1px solid $boundaryBlack;
+      border-radius: 8px;
+    }
+
+    > .title {
+      margin-top: -4px;
+    }
+
+    > .artist {
+      line-height: 1;
+      color: $gray;
+    }
+  }
+}
+</style>
