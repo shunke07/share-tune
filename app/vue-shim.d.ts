2e0f573654b7e9cd9207875b4867e1a2806b658f
shunke07 <metagurosu0@gmail.com>
2020-03-29 03:49:18 +0900

    Update shim.d.ts declare inject

--- app/vue-shim.d.ts
+++ app/vue-shim.d.ts
@@ -1,4 +1,10 @@
-declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+import Vue from 'vue'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $firebase: any
+    $firestore: any
+    $functions: any
+    $auth: any
+  }
 }
