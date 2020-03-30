7fcffc34eb01e17eafc82adf8b119ced9972ada2
shunke07 <metagurosu0@gmail.com>
2020-03-30 18:23:30 +0900

    Refactor store/index.js with ts

---
+++ app/store/index.ts
@@ -0,0 +1,28 @@
+import { ActionTree, MutationTree } from 'vuex'
+import { getUser } from '~/repositories/firestore/users'
+
+export const state = () => ({
+  isLoading: false as boolean,
+  loginUser: null as any
+})
+
+export type RootState = ReturnType<typeof state>
+
+export const mutations: MutationTree<RootState> = {
+  setIsLoading(state, payload: boolean) {
+    state.isLoading = payload
+  },
+  setLoginUser(state, payload: any) {
+    state.loginUser = payload
+  }
+}
+
+export const actions: ActionTree<RootState, RootState> = {
+  async login({ commit }, { uid }: { uid: string }) {
+    const user = await getUser({ uid })
+    if (!user) return
+
+    commit('setLoginUser', user)
+    this.$router.push('/releases/')
+  }
+}
