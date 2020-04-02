d18a537f2eac65278c983437da7ef09d5258cd56
shunke07 <metagurosu0@gmail.com>
2020-04-02 18:00:37 +0900

    Update query arguments

--- app/repositories/firestore/bookmarks/index.ts
+++ app/repositories/firestore/bookmarks/index.ts
@@ -1,10 +1,15 @@
 import { bookmarksRef, timestamp } from '~/repositories/firestore/config.ts'
 import { Bookmark } from '~/types/firestore'
 
-type Payload = { uid: string } & Bookmark['album']
+type Payload = {
+  uid: string
+  album: Bookmark['album']
+}
 
 export const createBookmark = async (payload: Payload) => {
-  const { uid, id, imageUrl, name, artist } = payload
+  const { uid, album } = payload
+  const { id, imageUrl, name, artist } = album
+
   await bookmarksRef(uid).add({
     album: {
       id,
