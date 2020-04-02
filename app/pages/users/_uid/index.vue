51bcb89936e6338b562c7669d7f0f1149eeb0b27
shunke07 <metagurosu0@gmail.com>
2020-04-02 23:08:58 +0900

    Update users views SectionProfile

--- app/pages/users/_uid/index.vue
+++ app/pages/users/_uid/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <div />
+  <div v-if="user" class="container">
+    <SectionProfile :user="user" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import SectionProfile from '~/components/users/SectionProfile.vue'
+import { User } from '~/types/firestore'
+
+export default Vue.extend({
+  components: {
+    SectionProfile
+  },
+
+  computed: {
+    user(): User {
+      return this.$store.state.loginUser
+    }
+  }
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  margin-top: 56px;
+}
+</style>
