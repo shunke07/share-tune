387a44374e005ee6be042e6863c81de7231b2a39
shunke07 <metagurosu0@gmail.com>
2020-04-02 11:33:13 +0900

    Update app/spotify-api.d.ts import from functions/types

--- app/types/spotify-api.d.ts
+++ app/types/spotify-api.d.ts
@@ -1,35 +1,4 @@
-type Item = {
-  album_type: string
-  artists: { key: string }[]
-  external_urls: { key: string }
-  id: string
-  images: string[]
-  name: string
-  release_date: string
-}
+import { Item, Track, Album } from '../../functions/src/types/spotify-api'
 
 export type Albums = readonly Item[]
-
-type Artists = {
-  name: string
-}
-
-type Track = {
-  artists: { key: string }[]
-  external_urls: { key: string }
-  id: string
-  name: string
-  preview_url: string
-  track_number: number
-}
-
-export type Album = {
-  album_type: string
-  artists: Artists[]
-  external_urls: { key: string }
-  id: string
-  images: string[]
-  name: string
-  release_date: string
-  tracks: { items: Track[] }
-}
+export { Item, Track, Album }
