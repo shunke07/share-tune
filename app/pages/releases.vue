00e49cf691c17e34e3049ed4c10ae17a0e965a18
shunke07 <metagurosu0@gmail.com>
2020-04-02 22:28:56 +0900

    Update AppBar leading to users

--- app/pages/releases.vue
+++ app/pages/releases.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="container">
+    <nav class="leading">
+      <button class="icon" @click="pushToMyPage()">
+        <svg-icon name="person" title="my-page" />
+      </button>
+    </nav>
     <ListReleases />
   </div>
 </template>
@@ -15,6 +20,15 @@ export default Vue.extend({
     ListReleases
   },
 
+  methods: {
+    pushToMyPage() {
+      const uid = this.$firebase.currentUser?.uid
+      if (!uid) return
+
+      this.$router.push(`/users/${uid}/`)
+    }
+  },
+
   head(): MetaInfo {
     return {
       title: 'リリース'
@@ -26,5 +40,26 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .container {
   margin-top: 56px;
+
+  > .leading {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 16px;
+    height: 44px;
+    z-index: 3;
+
+    > .icon {
+      width: 24px;
+      height: 24px;
+
+      > svg {
+        width: 100%;
+        height: 100%;
+        color: $gray;
+      }
+    }
+  }
 }
 </style>
