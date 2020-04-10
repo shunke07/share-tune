9cc881f5866002c973d938604fe23b6a82fcbcf6
shunke07 <metagurosu0@gmail.com>
2020-04-10 23:59:05 +0900

    Update AppBar action to settings page

--- app/pages/users/_uid/index.vue
+++ app/pages/users/_uid/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div v-if="user" class="container">
+    <nav class="leading">
+      <button class="icon" @click="$router.push('/settings/')">
+        <svg-icon name="actions/settings" title="プロフィール設定" />
+      </button>
+    </nav>
     <SectionProfile :user="user" class="profile" />
     <ListBookmarks />
   </div>
@@ -33,6 +38,30 @@ export default Vue.extend({
   > .profile {
     border-bottom: 1px solid $boundaryBlack;
     margin-bottom: 16px;
+  }
+
+  > .leading {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    max-width: $maxViewWidth;
+    height: 44px;
+    padding-right: 16px;
+    z-index: 3;
+
+    > .icon {
+      width: 24px;
+      height: 24px;
+
+      > svg {
+        color: $gray;
+        width: 100%;
+        height: 100%;
+      }
+    }
   }
 }
 </style>
